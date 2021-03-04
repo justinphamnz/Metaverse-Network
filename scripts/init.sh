5920ba2a137d09da9702a921421ad682e68a1066
justinphamnz <justin.phamnz@gmail.com>
2021-03-04 21:23:30 +1300

    update nightly to solve the conflict

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,8 +5,11 @@ set -e
 echo "*** Initializing WASM build environment"
 
 if [ -z $CI_PROJECT_NAME ] ; then
-   rustup update nightly
-   rustup update stable
+    rustup update nightly
+    rustup update nightly-2020-10-06
+    rustup update stable
+    rustup default nightly-2020-10-06
 fi
 
 rustup target add wasm32-unknown-unknown --toolchain nightly
+rustup target add wasm32-unknown-unknown --toolchain nightly-2020-10-06
